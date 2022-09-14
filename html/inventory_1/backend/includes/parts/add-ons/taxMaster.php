1222b293be985e99e6f577e138bb789cde68ab80
Uyin Solomon <rootech1@hotmail.com>
2022-09-14 08:13:46 +0000

    changing web roots

--- html/inventory_1/backend/includes/parts/add-ons/taxMaster.php
+++ html/inventory_1/backend/includes/parts/add-ons/taxMaster.php
@@ -5,28 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SMHOS - CLI</title>
-    <link rel="stylesheet" href="../../../../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../../../css/anton.css">
-    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/anton.css">
+    <link rel="stylesheet" href="/css/all.css">
     <link rel="stylesheet" href="css/keyboard.css">
     <link rel="icon" type="image/png" href="/assets/logo/logo.ico">
 
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/classes/session.js"></script>
-    <script src="js/classes/j_query_supplies.js"></script>
-    <script src="js/classes/db_trans.js"></script>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/classes/session.js"></script>
+    <script src="/js/classes/j_query_supplies.js"></script>
+    <script src="/js/classes/db_trans.js"></script>
 
-    <script src="js/sweetalert2@11"></script>
-    <link rel="stylesheet" href="css/sweetalert.min.css">
+    <script src="/js/sweetalert2@11"></script>
+    <link rel="stylesheet" href="/css/sweetalert.min.css">
 
-    <script src="js/error_handler.js"></script>
-    <script src="js/anton.js"></script>
-    <script src="js/keyboard.js"></script>
-    <script src="js/query.js"></script>
-    <script src="js/classes/buttons.js"></script>
+    <script src="/js/error_handler.js"></script>
+    <script src="/js/anton.js"></script>
+    <script src="/js/keyboard.js"></script>
+    <script src="/js/query.js"></script>
+    <script src="/js/classes/buttons.js"></script>
 
 
 
