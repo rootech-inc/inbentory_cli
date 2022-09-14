6fecb2403ba330dd323451805f0c4dd972a06340
Uyin Solomon <rootech1@hotmail.com>
2022-09-14 08:12:56 +0000

    tax screen added

---
+++ html/inventory_1/backend/includes/parts/add-ons/taxMaster.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMHOS - CLI</title>
+    <link rel="stylesheet" href="../../../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../../css/anton.css">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/keyboard.css">
+    <link rel="icon" type="image/png" href="/assets/logo/logo.ico">
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/classes/session.js"></script>
+    <script src="js/classes/j_query_supplies.js"></script>
+    <script src="js/classes/db_trans.js"></script>
+
+    <script src="js/sweetalert2@11"></script>
+    <link rel="stylesheet" href="css/sweetalert.min.css">
+
+    <script src="js/error_handler.js"></script>
+    <script src="js/anton.js"></script>
+    <script src="js/keyboard.js"></script>
+    <script src="js/query.js"></script>
+    <script src="js/classes/buttons.js"></script>
+
+
+
+
+
+
+
+
+</head>
+
+<body style="height: 100vh; overflow: hidden">
+    <header>
+        <button class="btn btn-info rounded-0 btn-sm">New Tax Group</button>
+    </header>
+    <article>
+        <table class="table table-sm table-hover">
+            <thead>
+                <tr>
+                    <th>Code</th>
+                    <th>Description</th>
+                    <th>Rate</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>VM</td>
+                    <td>VAT AND LEVIES</td>
+                    <td>17%</td>
+                </tr>
+            </tbody>
+        </table>
+    </article>
+</body>
+
+</html>
